70930601b85305e214af1af2ebdc72e0a299abf1
Zefiryn <zefiryn@jewula.net>
2014-07-04 19:42:42 +0200

    Generate xml string in js instead of php;read existing xml file and import data to table

--- assets/js/table.js
+++ assets/js/table.js
@@ -4,17 +4,20 @@ function SidecarRow(row) {
     self.row = ko.observable(row);
 }
 
-// Overall viewmodel for this screen, along with initial state
-function RowsViewModel() {
+// Overall viewmodel for this screen, along with the initial state
+function SidecarViewModel() {
     var self = this;
     
-    self.sourceFormats = ko.observableArray(['InDesign', 'HTML'])
+    self.sourceFormats = ko.observableArray([
+            {value: 'indd', label: 'InDesign'},
+            {value: 'html', label: 'HTML'}
+        ]);
 
     // Empty row with default data
     self.fileRows = [
         {
           article_name: "",
-          source_format : ['InDesign'],
+          source_format : ['indd'],
           custom_toc_icon : "",
           vertical_file_location : "",
           vertical_file_layout_name : "",
@@ -42,7 +45,7 @@ function RowsViewModel() {
     // Add new row to the table
     self.addNewRow = function() {
       //create new instance of this model to get default fileRow values
-      var newViewModel = new RowsViewModel();
+      var newViewModel = new SidecarViewModel();
       self.rowsCollection.push(new SidecarRow(newViewModel.fileRows[0]));
     };
     
@@ -52,16 +55,87 @@ function RowsViewModel() {
     
     // Generate xml file
     self.generateSidecar = function() {
-      var rowsToSend = {items: []};
+      var sidecarObj = {sidecar: {entry: []}};
+      var x2js = new X2JS({escapeMode: false});
       ko.utils.arrayForEach(self.rowsCollection(), function(item, idx) {
-        rowsToSend['items'][idx] = item.row();
+        var row = {
+          contentSource: {
+            articleName: item.row().article_name,
+            sourceFormat: item.row().source_format,
+            customTocIcon: item.row().custom_toc_icon,
+            sourceFile_v: {
+              location: item.row().vertical_file_location,
+              layoutName: item.row().vertical_file_layout_name
+            },
+            sourceFile_h: {
+              location: item.row().horizontal_file_location,
+              layoutName: item.row().horizontal_file_layout_name
+            }
+          },
+          articleTitle: item.row().article_title,
+          author: item.row().author,
+          kicker: item.row().kicker,
+          description: item.row().description,
+          tags: item.row().tags,
+          isAd: item.row().ads,
+          smoothScrolling: item.row().smooth_scrolling,
+          isFlattenedStack: item.row().flattened_stack,
+          articleAccess: item.row().article_access
+        };
+        sidecarObj.sidecar.entry.push(row);
       });
-      var object = self;
-      $.get('backend/generate.php', rowsToSend, function(response){
-        object.generatedXML(response);
-      });      
+      
+      var sidecarXmlString = x2js.json2xml_str(sidecarObj);      
+      self.generatedXML(formatXml(sidecarXmlString));
+    };
+    
+    self.importSidecarData = function(xmlstr) {
+        var x2js = new X2JS({escapeMode: false});
+        var jsonObj = x2js.xml_str2json(xmlstr);
+        
+        self.rowsCollection.removeAll();
+        ko.utils.arrayForEach(jsonObj.sidecar.entry, function(item, idx) {
+            var contentSource = item.contentSource;
+            var sourceFile = {  v: contentSource.sourceFile_v !== undefined ? contentSource.sourceFile_v : {},
+                                h: contentSource.sourceFile_h !== undefined ? contentSource.sourceFile_h : {}};
+            var rowData = {article_name: contentSource.articleName,
+                            source_format : [contentSource.sourceFormat],
+                            custom_toc_icon : contentSource.customTocIcon,
+                            vertical_file_location : sourceFile.v.hasOwnProperty('location') ? sourceFile.v.location : '',
+                            vertical_file_layout_name : sourceFile.v.hasOwnProperty('layoutName') ? sourceFile.v.layoutName : '',
+                            horizontal_file_location : sourceFile.h.hasOwnProperty('location') ? sourceFile.h.location : '',
+                            horizontal_file_layout_name : sourceFile.h.hasOwnProperty('layoutName') ? sourceFile.h.layoutName : '',
+                            article_title : item.articleTitle,
+                            author : item.author,
+                            kicker : item.kicker,
+                            description : item.description,
+                            tags : item.tags,
+                            ads : item.isAd === 'true',
+                            smooth_scrolling : item.smoothScrolling,
+                            flattened_stack : item.isFlattenedStack === 'true',
+                            article_access : item.articleAccess
+                          };
+            self.rowsCollection.push(new SidecarRow(rowData));
+        });
+    };
+    
+    //read xmlfile provided to file input field
+    self.readXmlFile= function(obj, evt) {
+        var file = evt.target.files[0]; 
+        if (file.type.match('text.*')) {
+            var reader = new FileReader();
+            reader.onloadend = (function(f){
+                return function(e) {
+                    self.importSidecarData(e.target.result);
+                };
+            })(file);
+            reader.readAsText(file);
+        }
+        else {
+            alert('Incorrect file type');
+        }
     };
 }
 
-var viewModel = new RowsViewModel();
+var viewModel = new SidecarViewModel();
 ko.applyBindings(viewModel);
