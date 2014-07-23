6b11c7f87218ff727f74926759b343a69c40a61f
Zefiryn <zefiryn@jewula.net>
2014-07-23 21:25:43 +0200

    bugfix

--- license.php
+++ license.php
@@ -2,7 +2,7 @@
 
 $response = array('valid' => false);
 if (isset($_GET['id']) && isset($_GET['user'])) {
-    $reponse['valid'] = strstr($_GET['user'] , '@kobugi.pl') ? true : false;
+    $response['valid'] = strstr($_GET['user'] , '@kobugi.pl') ? true : false;
     $response['id'] = $_GET['id'];
     $response['user'] = $_GET['user'];
 }
